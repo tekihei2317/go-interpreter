11c5cef5efc2cd5d4ac70b7ae23b238ec038cef4
tekihei2317 <tekihei2317@gmail.com>
2022-07-27 14:52:54 +0900

    feat: 字句解析器(仮)を実装

---
+++ lexer/lexer.go
@@ -0,0 +1,62 @@
+package lexer
+
+import (
+	"github.com/tekihei2317/go-interpreter/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int // 最後に読んだ位置
+	readPosition int // 次に読む位置
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input, position: -1, readPosition: 0, ch: 0}
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) NextToken() token.Token {
+	l.readChar()
+
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	return tok
+}
---
+++ token/token.go
@@ -0,0 +1,24 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+)
